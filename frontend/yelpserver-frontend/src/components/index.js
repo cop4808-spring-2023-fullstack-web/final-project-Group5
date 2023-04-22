3fd97c6e22b94ed799276bd4f8a28341c45de02d
Kevin Horta <kevinhorta3@gmail.com>
2023-04-22 14:08:07 -0400

    Adding navbar and home page
    So the homepage consist of two components that matter
    
    Navbar.jsx and HeroSection.jsx
    
    navbar is our top
    hero section is just our our login/create account body in our homepage site
    
    feel free to change any css all icons are from font awesome poor people edition
    
    The buttons dont lead anywhere yet but have empty href tags for diffrent pages

--- frontend/yelpserver-frontend/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Navigation from './Navbar';
-import Search from './Search'
-
-export {
-  Navigation,
-  Search
-}
---
+++ odyssey/src/components/Button.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
+
+// Define the possible styles and sizes for the button
+const STYLES = ["btn-primary", "btn--outline"];
+const SIZES = ["btn--medium", "btn--large"];
+
+// Create the Button component
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+  // Check if the button style and size passed in are valid, otherwise use the defaults
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  // Render the button with the appropriate style, size, onClick function, and child content
+  return (
+    <Link to="/" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
