9418d435d01eaf6218f6ceebc4e4f0d883749b98
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-04-30 01:11:18 -0400

    match main

--- odyssey/src/components/VideoSection.jsx
+++ odyssey/src/components/VideoSection.jsx
@@ -9,21 +9,25 @@ const VideoSection = () => {
       <h1>Odyssey</h1>
       <p>Get ready to embark on an epic odyssey</p>
       <p>with our trip planner</p>
-      <div className="hero-btns text-center">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          Create an Account<i class="fa-solid fa-plus"></i>
-        </Button>
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          Sign in <i class="fa-solid fa-user"></i>
-        </Button>
+      <div className="hero-btns">
+        {authorized ? (
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Explore <i className="fa-solid fa-globe"></i>
+          </Button>
+        ) : (
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+            onClick={loginGoogle}
+          >
+            Get Started <i className="fa-solid fa-user"></i>
+          </Button>
+        )}
       </div>
     </div>
   );
