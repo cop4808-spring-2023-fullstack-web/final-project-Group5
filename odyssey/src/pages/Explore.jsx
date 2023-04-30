f3a08975727ce502d94dded14a67b9dda14d9d50
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-04-29 20:35:25 -0400

    moved bootstrap css to index.js

--- odyssey/src/pages/Explore.jsx
+++ odyssey/src/pages/Explore.jsx
@@ -1,33 +1,32 @@
-import { Button, Form, Card } from 
-'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, Card } from "react-bootstrap";
 
 export default function Explore() {
-  return(
+  return (
     <div>
-      <h1 className='text-center m-5 text-3xl'>Start Your Trip</h1>
-      <div className='flex justify-center m-5'>
-        <Card className='p-4 w-[600px]'>
+      <h1 className="text-center m-5 text-3xl">Start Your Trip</h1>
+      <div className="flex justify-center m-5">
+        <Card className="p-4 w-[600px]">
           <Form>
             <Form.Group className="mb-3" id="formDest">
               <Form.Label>Destination</Form.Label>
-              <Form.Control className='w-1/2 ' type="" placeholder="City or Zipcode" />
+              <Form.Control
+                className="w-1/2 "
+                type=""
+                placeholder="City or Zipcode"
+              />
               From:
-              <Form.Control id='dateFrom' type='date' />
+              <Form.Control id="dateFrom" type="date" />
               To:
-              <Form.Control id='dateTo' type='date' />
+              <Form.Control id="dateTo" type="date" />
             </Form.Group>
-
           </Form>
-        </Card> 
-        
+        </Card>
       </div>
-      <div className='flex justify-center'>
-        <Button className='' size='lg' variant="dark" type="submit">
+      <div className="flex justify-center">
+        <Button className="" size="lg" variant="dark" type="submit">
           Embark
-        </Button>  
+        </Button>
       </div>
-      
     </div>
-  )
+  );
 }
