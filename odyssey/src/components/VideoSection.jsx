dbbac26b6c3146218095f6030e41a35dbe58064a
Grant626 <grant626f@gmail.com>
2023-04-30 00:09:41 -0400

    frontend google firebase on home

--- odyssey/src/components/VideoSection.jsx
+++ odyssey/src/components/VideoSection.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./Button";
 import "./VideoSection.css";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from '../config/config'
 
 const VideoSection = () => {
+
+  const [authorized, setAuthorized] = useState(false)
+
+  const loginGoogle =  async (e) => {
+
+    signInWithPopup(auth, new GoogleAuthProvider())
+    .then((userCred) => {
+      if(userCred) {
+        setAuthorized(true);
+      }
+      console.log(userCred);
+    })
+  }
+
   return (
     <div className="video-container">
       <video src="/videos/video3.mp4" autoPlay loop muted />
@@ -10,20 +26,24 @@ const VideoSection = () => {
       <p>Get ready to embark on an epic odyssey</p>
       <p>with our trip planner</p>
       <div className="hero-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          Create an Account<i class="fa-solid fa-plus"></i>
-        </Button>
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          Sign in <i class="fa-solid fa-user"></i>
-        </Button>
+        {authorized ? (
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+          Explore <i className="fa-solid fa-globe"></i>
+          </Button>
+        ) : (
+          <Button
+            className="btns"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+            onClick={loginGoogle}
+          >
+          Get Started <i className="fa-solid fa-user"></i>
+          </Button>
+        )}
       </div>
     </div>
   );
