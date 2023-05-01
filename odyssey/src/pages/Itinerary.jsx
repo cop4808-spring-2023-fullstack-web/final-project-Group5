bcce17a439ddec42b6b77c97823ae66b19fbb368
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-04-30 20:49:46 -0400

    added axios to fill card info

--- odyssey/src/pages/Itinerary.jsx
+++ odyssey/src/pages/Itinerary.jsx
@@ -1,5 +1,7 @@
-import { BusinessCard } from "../components"
-import { useState, useEffect } from "react"
+import { BusinessCard } from "../components";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 export default function Itinerary(props) {
   //get auth token from state
@@ -8,11 +10,82 @@ export default function Itinerary(props) {
   const [authorized, setAuthorized] = useState(
     false || window.localStorage.getItem("auth") === "true"
   );
+  
+  // get query parameters
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  
+  // get destination, startDate, endDate from query parameters
+  const destination = queryParams.get("destination");
+  const startDate = queryParams.get("startDate");
+  const endDate = queryParams.get("destination");
+  
+  // Initialize states for business id's of place categories
+  const [hotelBizID, setHotelBizID] = useState("");
+  const [breakfastBizID, setBreakfastBizID] = useState("");
+  const [lunchBizID, setLunchBizID] = useState("");
+  const [activityBizID, setActivityBizID] = useState("");
+  const [dinnerBizID, setDinnerBizID] = useState("");
+  
   useEffect(() => {
     if(authToken) {
       setAuthorized(true);
     }
-  }, [authToken, setAuthorized])
+    // get Hotel from backend
+    axios.get(`http://localhost:8000/search/Hotel/${destination}`)
+      .then(response => {
+        if (response.data.length > 0) {
+          setHotelBizID(response.data[0].id);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    // get Breakfast from backend
+    axios.get(`http://localhost:8000/search/Breakfast/${destination}`)
+      .then(response => {
+        if (response.data.length > 0) {
+          setBreakfastBizID(response.data[0].id);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    
+    // get Lunch from backend
+    axios.get(`http://localhost:8000/search/Lunch/${destination}`)
+      .then(response => {
+        if (response.data.length > 0) {
+          setLunchBizID(response.data[0].id);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    
+    // get Activity from backend
+    axios.get(`http://localhost:8000/search/Activity/${destination}`)
+      .then(response => {
+        if (response.data.length > 0) {
+          setActivityBizID(response.data[0].id);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    // get Dinner from backend
+    axios.get(`http://localhost:8000/search/Dinner/${destination}`)
+    .then(response => {
+      if (response.data.length > 0) {
+        setDinnerBizID(response.data[0].id);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, [authToken, setAuthorized], [destination])
 
   return(
     <>
@@ -22,27 +95,27 @@ export default function Itinerary(props) {
       <div>
         <div className="flex flex-col justify-center">
           <p className="m-0 text-center">Hotel Recommendation:</p>
-          <BusinessCard bizID='VXLcx7inE7lsdxLA-8CZIA' token={authorized}/>
+          <BusinessCard bizID={hotelBizID} token={authorized}/>
         </div>
         
         <div className="flex flex-col justify-center">
           <p className="m-0 text-center">Breakfast Recommendation:</p>
-          <BusinessCard bizID='' />
+          <BusinessCard bizID={breakfastBizID} token={authorized}/>
         </div>
 
         <div className="flex flex-col justify-center">
           <p className="m-0 text-center">Lunch Recommendation:</p>
-          <BusinessCard bizID='' />
+          <BusinessCard bizID={lunchBizID} token={authorized}/>
         </div>
 
         <div className="flex flex-col justify-center">
           <p className="m-0 text-center">Activity Recommendation:</p>
-          <BusinessCard bizID='' />
+          <BusinessCard bizID={activityBizID} token={authorized}/>
         </div>
 
         <div className="flex flex-col justify-center">
           <p className="m-0 text-center">Dinner Recommendation:</p>
-          <BusinessCard bizID='' />
+          <BusinessCard bizID={dinnerBizID} token={authorized}/>
         </div>
 
       </div>
