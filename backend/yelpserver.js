fadb902bb5cbdc5cecb3cc0da40549b42d7d84aa
Grant626 <grant626f@gmail.com>
2023-04-30 22:44:41 -0400

    patch for future deployment, -config.js

--- backend/yelpserver.js
+++ backend/yelpserver.js
@@ -22,6 +22,12 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200
