d6f35758effb5d2968c1c5f44ecadf2212e7928a
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-05-01 02:20:12 -0400

    profile shows current name, email if authenticated

--- odyssey/src/pages/Profile.jsx
+++ odyssey/src/pages/Profile.jsx
@@ -2,12 +2,27 @@ import { Button } from "react-bootstrap"
 import { signOut, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 import { auth } from "../config/config";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { getAuth } from "firebase/auth";
 
 export default function Profile() {
-
+  
   const [authorized, setAuthorized] = useState(
     false || window.localStorage.getItem("auth") === "true"
   );
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  let displayName = '';
+  let email = '';
+  if (user !== null) {
+    // The user object has basic properties such as display name, email, etc.
+    displayName = user?.displayName;
+    email = user.email;
+  }
 
   useEffect(() => {
     auth.onAuthStateChanged((userCred) => {
@@ -46,13 +61,44 @@ export default function Profile() {
   return (
     <>
     {authorized ? (
-      <Button className="m-2" size="lg" variant="dark" onClick={logout}>
-        Logout <i className="fa-solid fa-sign-out"></i>
-      </Button>
+      // IF AUTHORIZED Content ot render goes here
+      <>
+        <div className="d-flex flex-row">
+          <div className="d-flex flex-column p-3" style={{width:'280px'}}>      
+            <Nav className="flex-column">
+              <Nav.Link as={Link} to={"/profile"} className="m-2 bg-dark rounded text-light mb-2 active">
+                <i class="fa-solid fa-user me-2"></i>
+                Profile
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/preferences"} className="m-2 bg-dark rounded text-light mb-2">
+                <i class="fa-solid fa-gear me-2"></i>
+                Preferences
+              </Nav.Link>
+            </Nav>
+            <Button className="m-2" size="lg" variant="dark" onClick={logout}>
+              Logout <i className="fa-solid fa-sign-out"></i>
+            </Button>
+          </div>
+          <div className="d-flex flex-column p-3">
+              <h3>Current User</h3>
+              <p>Username: {displayName}</p>
+              <p>Email: {email}</p>
+            </div>
+        </div>
+        
+        
+      </>
     ) : (
-      <Button className="m-2" size="lg" variant="dark" onClick={loginGoogle}>
-        Log in <i className="fa-solid fa-user"></i>
-      </Button>
+      // NOT AUTHORIZED Content goes here
+      <>
+        <div className="flex flex-column" style={{width:'280px'}}>
+          <Button className="m-2" size="lg" variant="dark" onClick={loginGoogle}>
+            Log in <i className="fa-solid fa-user"></i>
+          </Button>
+        </div>
+      </>
+      
+      
     )} 
     </>
   )
