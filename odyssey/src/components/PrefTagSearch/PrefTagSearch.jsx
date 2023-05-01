e306b24d8bd47e61c6b8682425e3f143ea6305d6
OmrM <omar12726@gmail.com>
2023-05-01 02:31:06 -0600

    added static objects for search preferences.

--- odyssey/src/components/PrefTagSearch/PrefTagSearch.jsx
+++ odyssey/src/components/PrefTagSearch/PrefTagSearch.jsx
@@ -4,10 +4,8 @@ import { Dropdown, Button, Form, Card } from 'react-bootstrap';
 import './PrefTagSearch.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faMinus, faTimes, faPlus, faTags, } from '@fortawesome/free-solid-svg-icons';
-//import { BiSearch, BiMinus, BiX, BiPlus, BiTag } from 'react-icons/bi';
 import Select from 'react-select';
-
-
+import { hotelTags, breakfastRestaurantTags, lunchRestaurantTags, activityTags, dinnerRestaurantTags } from './tagsData';
 
 /* 
 PrefTagSearch Component
@@ -25,42 +23,35 @@ const PrefTagSearch = ({ title, onUpdate }) => {
     //initial state for hiding search bar. set to false
     const [searchVisible, setSearchVisible] = useState(false);
 
-    //example object that holds tag data: 
-    const tagsData = [
-        { id: 1, label: "tag1", value: "tag1" },
-        { id: 2, label: 'tag2', value: 'tag2' },
-        { id: 3, label: 'Restaurants1', value: 'Restaurants1' },
-    ];
-
     useEffect(() => {
         //notify parent component when selected tags are updated: 
-        onUpdate(selectedTags);
-        setTags(tagsData);
-        tags.map(tag => console.log(tag.label));
-        fetchCategories();
-        tags.map(tag => console.log(tag.label));
-        //setTags(tagsData);
-
-
-
-        //the [title, selectedTag] is a dependency array for the useEffect hook
-        // useEffect hook is called when the dependencies' state changes (title and selectedTags are the dep.) 
+        onUpdate(title, selectedTags);
+        setTags(getTagsDataForTitle(title));
     }, [title, selectedTags]);
 
+    const getTagsDataForTitle = (title) => {
+        switch (title) {
+            case 'Hotel':
+                return hotelTags;
+            case 'Breakfast Restaurant':
+                return breakfastRestaurantTags;
+            case 'Lunch Restaurant':
+                return lunchRestaurantTags;
+            case 'Activity':
+                return activityTags;
+            case 'Dinner Restaurant':
+                return dinnerRestaurantTags;
+            default:
+                return [];
+        }
+    }
 
     //set search bar to initial state (not visible): 
     const toggleSearch = () => {
-        //setSearchVisible = !searchVisible;
         setSearchVisible(prevSearchVisible => !prevSearchVisible);
     };
 
     //function to add tags to the card component
-    /*     const addTag = (tag) => {
-            if (!selectedTags.includes(tag)) {
-                setSelectedTags([...selectedTags, tag]);
-            }
-        }; */
-    // Change addTag to check and store the whole tag object instead of just the value
     const addTag = (tag) => {
         // Check if tag object already exists in selectedTags
         if (!selectedTags.find(t => t.value === tag.value)) {
@@ -68,50 +59,18 @@ const PrefTagSearch = ({ title, onUpdate }) => {
             setSelectedTags([...selectedTags, tag]);
         }
     };
+
     // function to remove tags from card 
     const removeTag = (tag) => {
         setSelectedTags(selectedTags.filter(t => t !== tag));
-        //TODO: might have to remove the tag data from an array or something later
     };
 
     const saveTags = () => {
-        //TODO: add code to save the tag data to an array or something
-        //
-        //hide the searchbar once tags are saved
         toggleSearch();
     };
 
-    const fetchCategories = async() => {
-        try {
-            const endpoint = 'rest';
-            const response = await axios.get(`http://localhost:8000/auto/${endpoint}`, {
-                timeout: 5000
-            });
-            const categories = response.data.terms;
-    
-            // Create a new array with categories prepended to tagsData
-            const newTagsData = [
-                // Map each category (which is now an object) to a new tag object
-                ...categories.map((category, index) => ({
-                    id: `category-${index}`,
-                    label: category.text, // Here we access the 'text' property of the category
-                    value: category.text // And here as well
-                })),
-                ...tagsData
-            ];
-    
-            setTags(newTagsData);
-        } catch(error) {
-            console.error(error);
-        }
-    };
-    
-
-
     return (
         <Card className='p-4 w-[600px]'>
-
-
             <Card.Title className="card-header" style={{ textAlign: 'left' }}>{title} Preferences</Card.Title>
             {searchVisible && (
                 <div className="row-container">
@@ -120,20 +79,13 @@ const PrefTagSearch = ({ title, onUpdate }) => {
                         onChange={selectedOption => addTag(selectedOption)}
                         isSearchable
                         placeholder="Select Search Filters"
-                    /* allow searchbar to stretch to rem. width  */
-                    /* styles={{ container: (base) => ({ ...base, flexGrow: 1}) }} */
-
                     />
-
                     <div style={{ backgroundColor: 'transparent' }}>
                         <Button className='button' onClick={saveTags}>Save</Button>
                     </div>
                 </div>
             )}
-
             <div className='row-container'>
-
-
                 <div className="selected-tags">
                     {selectedTags.map(tag => (
                         <span key={tag.value} className="tag">
@@ -142,7 +94,6 @@ const PrefTagSearch = ({ title, onUpdate }) => {
                         </span>
                     ))}
                 </div>
-
 
 
                 {!searchVisible ? (
