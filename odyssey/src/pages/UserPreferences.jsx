11c52a2c978ba75229501cd1af6a72444858fd70
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-05-02 15:23:20 -0400

    moved file again

--- odyssey/src/pages/UserPreferences.jsx
+++ odyssey/src/pages/UserPreferences.jsx
@@ -3,7 +3,7 @@ import { signOut, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
-import { auth } from "../components/config/config";
+import { auth } from "../config/config";
 
 export default function UserPreferences() {
 
