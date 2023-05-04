bf72f4f74d0d608418be330353a01cae20a46a39
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-05-04 17:45:31 -0400

    should silence tailwind error messaga on build

--- odyssey/tailwind.config.js
+++ odyssey/tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      ...colors
+      ...colors,
+      sky: colors.lightBlue,
+      stone: colors.warmGray,
+      neutral: colors.trueGray,
+      gray: colors.coolGray,
+      slate: colors.blueGray,
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
