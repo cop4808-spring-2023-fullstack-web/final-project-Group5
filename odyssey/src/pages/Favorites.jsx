3256b05647944b14aaf19363c3ca0c2019d49504
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-05-04 15:02:23 -0400

    import favorites using new enpoint and auth

--- odyssey/src/pages/Favorites.jsx
+++ odyssey/src/pages/Favorites.jsx
@@ -1,7 +1,8 @@
-import { BusinessCard, LoginBtn } from "../components";
+import { LoginBtn } from "../components";
 import { useState, useEffect } from "react";
 import { getAuth } from "firebase/auth";
 import axios from "axios";
+import TestCard from "../components/TestCard";
 
 export default function Favorites(props) {
   //get auth token from state
@@ -24,15 +25,17 @@ export default function Favorites(props) {
     setUser(auth.currentUser);
 
     const loadFavorites = () => {
-      axios
-        .get(`/user/${user.uid}`)
-        .then((res) => {
-          setFavorites(res.data.favorites);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-        .then(() => {});
+      if (auth.currentUser) {
+        axios
+          .get(`/favorites/${auth.currentUser.uid}`)
+          .then((res) => {
+            setFavorites(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+          .then(() => {});
+      }
     };
 
     if (authorized) {
@@ -44,10 +47,13 @@ export default function Favorites(props) {
     <>
       {authorized ? (
         <div className="flex flex-col justify-center">
-          <h1 className="m-3 text-center">Your favorite businesses!</h1>
-          {favorites &&
-            favorites.length > 0 &&
-            favorites.map((biz) => <BusinessCard bizID={biz} />)}
+          <h1 className="m-5 text-center">Your favorite businesses!</h1>
+
+          <div className="row m-3 g-1">
+            {favorites &&
+              favorites.length > 0 &&
+              favorites.map((biz) => <TestCard business={biz} />)}
+          </div>
         </div>
       ) : (
         <div>
