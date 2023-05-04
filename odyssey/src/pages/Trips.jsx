3a761da81197e1d920994879cf67b3f9eec06df1
danram0121 <118145477+danram0121@users.noreply.github.com>
2023-05-04 15:03:43 -0400

    trips changes

--- odyssey/src/pages/Trips.jsx
+++ odyssey/src/pages/Trips.jsx
@@ -1,39 +1,54 @@
 import { useState, useEffect } from "react";
-import { LoginBtn } from '../components';
+import { LoginBtn } from "../components";
 
 export default function Explore(props) {
   //get auth token from state
-  const authToken = props.token
+  const authToken = props.token;
   //useState for checking authorization status
   const [authorized, setAuthorized] = useState(
     false || window.localStorage.getItem("auth") === "true"
   );
-  
+
   useEffect(() => {
-    if(authToken) {
+    if (authToken) {
       setAuthorized(true);
     }
-  }, [authToken, setAuthorized])
+  }, [authToken, setAuthorized]);
 
-  return(
+  return (
     <>
-    {authorized ? (
-      <div className="" style={{backgroundImage: "url('../images/lake.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}>
-        <div className="text-center h-screen flex flex-col justify-start items-center">
-          <h1 className="m-5">My Trips</h1>
-
+      {authorized ? (
+        <div
+          className=""
+          style={{
+            backgroundImage: "url('../images/lake.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="text-center h-screen flex flex-col justify-start items-center">
+            <h1 className="m-5">My Trips</h1>
+          </div>
         </div>
-      </div>
-    ) : (
-      <div className="" style={{backgroundImage: "url('../images/lake.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}>
+      ) : (
+        <div
+          className=""
+          style={{
+            backgroundImage: "url('../images/lake.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
           <div className="text-center h-screen flex flex-col justify-center items-center ">
-            <h1 className="text-3xl font-bold mb-5">Please Login to start your journey</h1>
+            <h1 className="text-3xl font-bold mb-5">
+              Please Login to start your journey
+            </h1>
             <div className=" p-5 ">
               <LoginBtn />
             </div>
           </div>
         </div>
-    )}
+      )}
     </>
-  )
+  );
 }
